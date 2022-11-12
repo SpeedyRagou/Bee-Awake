9e7002d540e255c5387c131acf4c0b1107f44b84
SpeedyRagou <m.f.kuhlmann@gmail.com>
2022-11-12 12:35:11 +0100

    implemented goal states

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GoalBehaviour : MonoBehaviour
@@ -12,18 +13,32 @@ public class GoalBehaviour : MonoBehaviour
     public int item_strenght;
     public int score_per_item;
 
+    private Sprite[] sleepState;
+    private int sleep_sprite;
+
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
         sleepiness = max_sleepiness;
+        sleepState = Resources.LoadAll<Sprite>("woke");
+        sleep_sprite = sleepState.Length - 1;
+        gameObject.GetComponent<SpriteRenderer>().sprite = sleepState[(sleepState.Length - 1) - sleep_sprite ];
     }
 
     // Update is called once per frame
     void Update()
     {
         sleepiness = sleepiness - sleepiness_change_rate * Time.deltaTime;
-        //Debug.Log(sleepiness);
+
+        float tmp_sprite = sleepiness / (max_sleepiness / (sleepState.Length - 1));
+        
+        if (Mathf.RoundToInt(tmp_sprite) != sleep_sprite)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = sleepState[Mathf.RoundToInt((sleepState.Length - 1) - tmp_sprite)];
+            sleep_sprite = Mathf.RoundToInt(tmp_sprite);
+        }
+        Debug.Log(sleep_sprite);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)

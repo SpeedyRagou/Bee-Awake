7883de19e70f26ee5245423b2394690997f626ec
SpeedyRagou <m.f.kuhlmann@gmail.com>
2022-11-12 22:42:11 +0100

    polishing

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -175,7 +175,7 @@ public class GoalBehaviour : MonoBehaviour
         //    }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Item")
         {

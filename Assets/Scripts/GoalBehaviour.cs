f055de9ccfe8266041c8186494a042bd65ac9f48
Malte Herrmann <plotzg@web.de>
2022-11-12 20:40:44 +0100

    Tag-Nacht-Cyclus implementiert Swap Images implementiert Alte Scripte entfernt

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Apple;
 using UnityEngine.SceneManagement;
 
 public class GoalBehaviour : MonoBehaviour
@@ -15,11 +17,19 @@ public class GoalBehaviour : MonoBehaviour
     public int score_per_item;
     public float movement_speed;
 
+    public float maxY = 5.5f;
+    public float minY = 2.5f;
+    public float maxX = -1.0f;
+    public float minX = -7.0f;
 
     private Sprite[] sleepState;
     private int sleep_sprite;
     private int movement_state;
     private Vector2 direction;
+
+    private List<Vector3> RandomTargetPositions = new List<Vector3>();
+
+    private bool turned = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,20 +37,32 @@ public class GoalBehaviour : MonoBehaviour
         sleepiness = max_sleepiness;
         sleepState = Resources.LoadAll<Sprite>("woke");
         sleep_sprite = sleepState.Length - 1;
-        gameObject.GetComponent<SpriteRenderer>().sprite = sleepState[(sleepState.Length - 1) - sleep_sprite ];
+        gameObject.GetComponent<SpriteRenderer>().sprite = sleepState[(sleepState.Length - 1) - sleep_sprite];
         movement_state = 0;
         direction = new Vector2(0, movement_speed);
+
+
+        for (float i = minY; i < maxY; i++)
+        {
+            for (float j = minX; j < maxX; j++)
+            {
+                Vector3 newVector = new Vector3(j, i, 0f);
+                RandomTargetPositions.Add(new Vector3(j, i, 0f));
+                Debug.Log(newVector);
+            }
+        }
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         sleepiness = sleepiness - sleepiness_change_rate * Time.deltaTime;
 
         float tmp_sprite = sleepiness / (max_sleepiness / (sleepState.Length - 1));
 
 
-        
+
 
         // ending check
         if (sleepiness <= 0)
@@ -58,18 +80,20 @@ public class GoalBehaviour : MonoBehaviour
             sleep_sprite = Mathf.RoundToInt(tmp_sprite);
         }
 
-        
+
         // select appropriate movement
-        if(score == 10)
+        if (score == 10)
         {
             movement_state = 1;
-        }else if(score == 20) {
+        }
+        else if (score == 20)
+        {
             movement_state = 2;
         }
 
 
         // up down movement
-        if(movement_state == 1)
+        if (movement_state == 1)
         {
             gameObject.GetComponent<Rigidbody2D>().velocity = direction;
 
@@ -83,43 +107,76 @@ public class GoalBehaviour : MonoBehaviour
             {
                 direction[1] = movement_speed;
                 gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition[0], -5.5f, 10);
+
             }
         }
 
         // down right left up movement
         if (movement_state == 2)
         {
-            
+
             gameObject.GetComponent<Rigidbody2D>().velocity = direction;
 
             if (gameObject.transform.localPosition[1] >= 2.5)
             {
                 direction[1] = movement_speed * -1;
                 gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition[0], 2.5f, 10);
+                turned = true;
             }
 
-            
+
             if (gameObject.transform.localPosition[1] <= -5.5 && gameObject.transform.localPosition[0] <= -7)
             {
-                Vector3 tmp = new Vector3(0, 0, 10);
-                tmp[0] = direction[1] * -1;
-                tmp[1] = direction[0] * -1;
-                direction = tmp;
-                gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition[0], -5.49f, 10);
+                if (turned)
+                {
+                    Debug.Log("hier");
+                    Vector3 tmp = new Vector3(0, 0, 10);
+                    tmp[0] = direction[1] * -1;
+                    tmp[1] = direction[0] * -1;
+                    direction = tmp;
+                    gameObject.transform.localPosition = new Vector3(-7f, -5.5f, 10f);
+                    turned = false;
+                }
             }
 
             if (gameObject.transform.localPosition[0] >= -1)
             {
                 direction[0] = movement_speed * -1;
-                gameObject.transform.localPosition = new Vector3(-1.01f, -5.5f, 10);
+                gameObject.transform.localPosition = new Vector3(-1.0f, -5.5f, 10f);
+                turned = true;
             }
         }
 
+        //    bool targetReached = true;
+        //    Vector3 destination = transform.position;
+        //    //Random Movement
+        //    if (movement_state == 1)
+        //    {
+        //        if (!targetReached)
+        //        {
+        //            int random = Random.Range(0, RandomTargetPositions.Count);
+        //            destination = RandomTargetPositions[random];
+        //            Debug.Log("Destination: " + destination);
+        //            targetReached = false;
+        //        }
+        //        //Pick random Destination
+
+        //        Vector3 directionMalte = transform.position - destination;
+
+
+        //        transform.position += directionMalte * Time.deltaTime;
+        //        Debug.Log("Position: " + transform.position);
+        //        if (Mathf.Abs(transform.position[0]) >= Mathf.Abs(destination[0]) && Mathf.Abs(transform.position[1]) >= Mathf.Abs(destination[1]))
+        //        {
+        //            targetReached = true;
+        //        }
+
+        //    }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Item")
+        if (collision.gameObject.tag == "Item")
         {
             if (collision.gameObject.GetComponent<Object>().good)
             {
@@ -145,5 +202,5 @@ public class GoalBehaviour : MonoBehaviour
         Debug.Log(score);
     }
 
-    
+
 }
---
+++ Assets/Scripts/SwapImages.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapImages : MonoBehaviour
+{
+    public Image StartImage;
+   // public Image EndImage;
+    float startTime;
+    public float elapsedTime;
+    float transparency = 1.0f;
+    public float fadeRate = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       startTime =  Time.unscaledTime;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transparency -= fadeRate;
+        if (Time.time > startTime + elapsedTime)
+        {
+            transparency -= fadeRate;
+            if (transparency > 0.0f)
+            {
+                Color color = StartImage.color;
+                StartImage.color = color.WithAlphaMultiplied(transparency);
+            }
+            else if (transparency < 0.0f)
+            {
+                StartImage.enabled = false;
+                GameObject.Find("Main Camera/Canvas/Background").SetActive(false);
+            }
+        }
+        
+    }
+}

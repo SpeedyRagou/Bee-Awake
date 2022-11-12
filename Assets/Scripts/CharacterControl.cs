2e3a609816d80d3445eda706ff55cd58bf7feb87
SpeedyRagou <m.f.kuhlmann@gmail.com>
2022-11-12 01:46:58 +0100

    added throwing

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
 
+    public float throwStrenght;
+    public float rotationStrenght;
+
     bool canCatch;
     GameObject catchable;
+    bool throwing;
+    float power;
+
 
     // Start is called before the first frame update
     void Start()
     {
         canCatch = false;
-        gameObject.transform.localScale = new Vector3(gameObject.transform.parent.localScale[1], gameObject.transform.parent.localScale[1], gameObject.transform.parent.localScale[1])
+        throwing = false;
+        power = 0;
+        //gameObject.transform.localScale = new Vector3(gameObject.transform.parent.localScale[1], gameObject.transform.parent.localScale[1], gameObject.transform.parent.localScale[1])
     }
 
     // Update is called once per frame
@@ -22,8 +30,19 @@ public class CharacterControl : MonoBehaviour
         {
             Debug.Log("Rotate arm");
             gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition[0] * -1, gameObject.transform.localPosition[1], 0);
-            gameObject.transform.rotation = Quaternion.AngleAxis(180, new Vector3(0, 0, 1));
+            if (throwing)
+            {
+                gameObject.transform.rotation = Quaternion.AngleAxis(0, new Vector3(0, 1, 0));
+                power = 0;
+            }
+            else
+            {
+                gameObject.transform.rotation = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
+            }
+            
+            throwing = !throwing;
         }
+
         if (canCatch)
         {
             if (Input.GetButtonDown("Jump"))
@@ -35,8 +54,6 @@ public class CharacterControl : MonoBehaviour
                     catchable.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
                     catchable.GetComponent<Rigidbody2D>().angularVelocity = 0f;
                     catchable.transform.parent = gameObject.transform;
-                    catchable.transform.localScale= new Vector3(1, 1/gameObject.transform.localScale[1], 1);
-                    catchable.transform.sca
 
                     
                 }
@@ -46,13 +63,36 @@ public class CharacterControl : MonoBehaviour
             {
                 if (catchable != null)
                 {
+                    
                     Debug.Log("Let item go");
-                    catchable.transform.parent = GameObject.Find("Main Camera/ObjectSpawner").transform;
-                    catchable.transform.localScale = Vector3.one;
                     catchable.GetComponent<Rigidbody2D>().isKinematic = false;
+                    catchable.transform.parent = GameObject.Find("Main Camera/ObjectSpawner").transform;
+                    if (throwing)
+                    {
+                        catchable.GetComponent<Rigidbody2D>().velocity = new Vector3(-power, power, 0);
+                        catchable.GetComponent<Rigidbody2D>().angularVelocity= rotationStrenght;
+                    }
                 }
             }
 
+        }
+
+
+        if (throwing && canCatch)
+        {
+            if(power >= 14)
+            {
+                power = 0;
+            }
+            else
+            {
+                power = Time.deltaTime * throwStrenght + power;
+                if (power > 14)
+                {
+                    power = 14;
+                }
+            }
+            Debug.Log(power);
         }
         
     }

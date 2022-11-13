d309abff74f38b20b9b246eb745375fd2e18d062
SpeedyRagou <m.f.kuhlmann@gmail.com>
2022-11-13 11:31:31 +0100

    settigns menu

---
+++ Assets/Scripts/RemappingBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemappingBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode Rotate;
+    public KeyCode Jump;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void remapButton(string buttontag, string buttoncode)
+    {
+        Input.
+    }
+}
